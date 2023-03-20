2794be0d77cf8e71bcc03ebfa75fe5e64b05362c
Fahad Kiani <fahadkiani@fahads-mbp.lan>
2023-03-19 21:40:03 -0400

    changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -43,7 +43,7 @@ const Home = () => {
       <Services />
       {/* <Welcome /> */}
       
-      <WhatWeDo/>
+      {/* <WhatWeDo/> */}
       {/* <Testimonials /> */}
       <Contact />
     </Container>
